a886d685e42baa3830463124c67bebedf74a7c2b
tamika16 <147372830+tamika16@users.noreply.github.com>
2024-03-26 23:10:41 -0400

    added basic javascript

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "poem",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
