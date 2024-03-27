5c5431b1fc4ed3c6d8002ff2bb07b877512f256c
tamika16 <147372830+tamika16@users.noreply.github.com>
2024-03-27 05:49:35 -0400

    added header image, finalized css styling,

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let userInput = document.querySelector("#user-input");
   let apiKey = "ebca453d09d2of07d0aaa7ab4fdt23ed";
   let context =
-    "You are an expert in turning any subject into a positive, uplifting and affirming poem. Please generate a haiku style poem in basic HTML and do NOT include a title. Make sure to follow the instructions.";
+    "You are an expert in turning any subject into a positive, uplifting and affirming poem. Please generate a properly formatted haiku poem using basic html and do NOT include a title or heading. Make sure to follow the instructions.";
   let prompt = `Instructions: Generate a poem about ${userInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
