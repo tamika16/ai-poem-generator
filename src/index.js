81dcec5fb72de3dd54f8a271da9e11594b8b76e0
tamika16 <147372830+tamika16@users.noreply.github.com>
2024-03-27 00:18:32 -0400

    updated titles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "ebca453d09d2of07d0aaa7ab4fdt23ed";
+  let context =
+    "You are an expert in turning any subject into a positive, uplifting and affirming poem. Please generate a haiku style poem in basic HTML and do NOT include a title. Make sure to follow the instructions.";
+  let prompt = `Instructions: Generate a poem about ${userInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
